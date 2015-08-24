5c913f20f912c34e457691ebe28325c758b0bf8a
maitzeth <andre801mz@gmail.com>
2015-08-24 00:14:30 -0430

    Agrega nuevos cambios, editar y ver boletin y en el index.php

--- vista/index.php
+++ vista/index.php
@@ -16,18 +16,46 @@ include_once "menu.php"
 					<div class="row">
 						<div class="col-md-12">
 							<div class="box box-danger">
-								<div class="box-header">
-									<h3 class="box-title">Sistema {#Nombre}</h3>
-								</div>
 								<div class="box-body">
-									
-									<p>Aqui va una breve descripcion del proyecto/sistema e integrantes</p>
-									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod quaerat labore laudantium amet qui sapiente voluptate odit autem, id. Doloremque impedit laudantium tempore vitae consequuntur ad! Laborum libero magni dicta.</p>
+									<h1 class="text-center">RESEÑA HISTORICA:</h1>
+									<p class="text-justify">UNIDAD EDUCATIVA NACIONAL INTEGRAL BOLIVARIANA  MARTIN JOSE SANABRIA.</p>
+									<p class="text-justify">La Escuela “MARTÍN JOSÉ SANABRIA” fue inaugurada el 7 de enero de 1930 con el nombre de “ARTURO MICHELENA” en una pequeña casa situada en Monte Piedad.</p>
+									<p class="text-justify">En 1932 se logra la construcción de un pequeño local en la calle Colombia y para la inauguración el Ministerio de Educación cambia el nombre de la escuela por el que actualmente tiene de acuerdo con la solicitud efectuada por la directora tomando en cuenta la importancia que tuvo este personaje bajo la presencia de Guzmán Blanco en la instrucción del pueblo venezolano.</p>
+									<p class="text-justify">
+									    Nuestra insigne institución lleva el nombre del Doctor” Martín José Sanabria” quien nació en Caracas el 15 de agosto de 1831 y falleció en la misma ciudad lleno de méritos y grandezas en el año 1904. Por su actuación relevante dentro del campo de la enseñanza, el Doctor Martín José Sanabria ha merecido el nombre del Apóstol de la Educación Venezolana; a el se debe la elaboración del decreto sobre la enseñanza gratuita y obligatoria, promulgado por el General Antonio Guzmán Blanco el 27 de junio de 1870.
+									</p>
+									<p class="text-justify">
+									    A comienzos de 1965 por año y medio es transferida a los módulos ubicados entre los bloques 5 y 6 del 23 de enero, donde actualmente funciona la escuela “Diego de Lozada” mientras era construido por el Ministerio de Obras Públicas (M.O.P) el nuevo local de tres niveles que ahora existe.
+									</p>
+									<p class="text-justify">
+									    La escuela fue reinaugurada el 21 de junio de 1966 contando con la presencia de la Directora Fundadora Lola Caruja. Más reciente en 2001 el plantel fue incorporado al proyecto de Escuelas Bolivarianas, se imparte Educación Inicial y Primaria, especialidades como Folklore, Música y Psicopedagogía. A partir del año 2009 cuenta con el Centro de Informática y Telemática (CBIT) donde los estudiantes mediante el uso de las Tecnologías de Información, y Comunicación (Las TIC) refuerzan los contenidos impartidos en clase.
+									</p>
+									<p class="text-justify">
+									    Actualmente con sus 82 años de fundada la escuela atiende a una población de mas de 526 alumnos teniendo un horario integral de 7:30am a 3:30pm. 
+									</p>
 								</div>
+							</div>
+							<div class="box box-danger">
+                            <div class="box-body">
+                                <h1 class="text-center">MISIÓN</h1>
+							    <p class="text-justify">
+							        La U.E.N.I.B. Martín José Sanabria tiene como misión ofrecer una educación de calidad que contribuya a la formación integral de nuestros estudiantes, promoviendo, orientando y generando en forma continua a un conjunto de acciones, experiencias y actividades pedagógicas dirigidas a la integración de todos los actores del hecho educativo, permitiendo así el desarrollo pluridimensional de todas las potencialidades del educando y el logro en la adquisición de nuevos conocimientos, el desarrollo de valores humanos para un aprendizaje significativo y productivo al servicio de su comunidad y la sociedad.
+							    </p>
+                            </div>
+							</div>
+							<div class="box box-danger">
+                            <div class="box-body">
+                                <h1 class="text-center">
+				                     VISIÓN 
+				                </h1>
+				                <p class="text-justify">
+							   La U.E.N.I.B. Martín José Sanabria tiene como visión ser una Institución Educativa, que busque permanentemente la excelencia en la formación integral del ser humano con base en los principios Bolivarianos y comprometidos con los valores de la justicia, equidad, respeto, responsabilidad, solidaridad y libertad como bases fundamentales en el desarrollo del estudiante como ser social. 
+							</p>
+                            </div>
+				                
 							</div>
 						</div>
 					</div>
-				</section>
 			</div>
 			<!-- Fin de Contenido -->
 			<footer class="main-footer">
