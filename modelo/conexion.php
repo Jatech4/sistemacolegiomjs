d4756af918ba8a1c4b96c4aab72a63f433c689ee
Junior Ayala <elessar303@gmail.com>
2015-10-07 22:34:39 -0430

    este se me olvido

--- modelo/conexion.php
+++ modelo/conexion.php
@@ -1,5 +1,11 @@
 <?php
-$link=mysql_connect("localhost", "root", '');
-mysql_select_db("martinjsanabria",$link) OR DIE ("Error: No es posible establecer la conexión");
+$dbhost = 'localhost';
+$dbuser = 'root';
+$dbpass = '123456';
+$dbname='martinjsanabria';
+$link=mysql_connect($dbhost, $dbuser, $dbpass);
+mysql_select_db($dbname,$link) OR DIE ("Error: No es posible establecer la conexión");
 mysql_query ("SET NAMES 'utf8'");
+//error_reporting(E_ALL);
+error_reporting(E_ALL & ~E_NOTICE);
 ?>
